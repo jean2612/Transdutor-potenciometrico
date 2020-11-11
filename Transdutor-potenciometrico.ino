32124088106a3b73ffcf7fc2553b7623ae092724
Jean Cimirro <49345210+jean2612@users.noreply.github.com>
2020-11-10 23:07:40 -0300

    Final, mostrando valor lido em voltes

--- Transdutor-potenciometrico.ino
+++ Transdutor-potenciometrico.ino
@@ -3,7 +3,7 @@
 //Define os pinos que serão utilizados para ligação ao display
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
-   Serial.begin(19200); //Inicializa o monitor serial com atualização de 19200bits
+  // Serial.begin(19200); //Inicializa o monitor serial com atualização de 19200bits
    lcd.begin(16, 2);
 
 }
@@ -13,14 +13,17 @@ void loop() {
   
   int valorPortaAnalogica = analogRead(A1); //recebe o valor da porta analógica A1
   float voltagem = valorPortaAnalogica*(5.00/1023.00); //converte valor em bits da porta analógica para um valor em volts
-  Serial.println(voltagem); //exibe no monitor serial o valor da voltagem;
+  //Serial.println(voltagem); //exibe no monitor serial o valor da voltagem;
    //Limpa a tela
   //lcd.clear();
-  lcd.setCursor(3, 0); //coloca um espaçamento e seleciona primeira linha
+  lcd.setCursor(0, 0); //coloca um espaçamento e seleciona primeira linha
   lcd.print("Vout="); //imprimi mensagem no lcd
-  lcd.setCursor(3, 1); //coloca um espaçamento e seleciona segunda linha
+  lcd.setCursor(5, 0); //coloca um espaçamento e seleciona segunda linha
   lcd.print(voltagem); //imprimi o valor lido pela porta analógica(A1) e exibe no lsd
   lcd.print("V");
+  lcd.setCursor(0,1);
+  lcd.print("Vbits=");
+  lcd.print(valorPortaAnalogica);
   }
 
 
